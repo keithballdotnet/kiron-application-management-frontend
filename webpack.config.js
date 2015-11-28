c1ac5b23d60130a57bbc648ccb05d6e2b9e9824e
lirsacc <c.lirsac@gmail.com>
2015-11-28 12:26:04 +0100

    Base npm project

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  devtool: 'sourcemap',
+
+  entry: {
+    index: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle-[hash].js'
+  },
+
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.css', '.scss']
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js[x]?$/, exclude: /node_modules/, loader: 'babel'
+      }, {
+        test: /\.json?$/, loader: 'json-loader'
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextWebpackPlugin.extract(
+          'css?sourceMap' +
+          '!autoprefixer-loader?browsers=last 2 versions' +
+          '!sass?sourceMap=true&sourceMapContents=true&outputStyle=compressed')
+      }
+    ],
+  },
+
+  plugins: [
+    // new webpack.NoErrorsPlugin(),
+    new ExtractTextWebpackPlugin('style-[hash].css', {
+        allChunks: true
+    }),
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+      inject: 'body' // Inject all scripts into the body
+    })
+  ],
+}
