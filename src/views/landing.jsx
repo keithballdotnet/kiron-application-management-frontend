770651f1d6eeabc2a3f05f3ede6f70826429538b
Sebastian Wolf <zapping-seb@Sebastians-MacBook-Pro-3.local>
2015-11-29 12:39:04 +0100

    ButtonsDone

--- src/views/landing.jsx
+++ src/views/landing.jsx
@@ -19,7 +19,7 @@ function LandingLink (props) {
   }
   if (to) {
     return (
-      <Link to={to} className='btn-primary btn sm-col-12 block col-12 mb2 center'>
+      <Link to={to} className='caps regular btn-primary btn sm-col-12 block col-12 mb2 center'>
         {text}
       </Link>
     );
@@ -31,12 +31,13 @@ function LandingLink (props) {
     }
     return (
       <a href="#" onClick={_click}
-         className='btn-primary btn sm-col-12 block col-12 mb2 center'>
+         className='caps regular btn-primary btn sm-col-12 block col-12 mb2 center'>
         {text}
       </a>
     );
   }
 }
+
 
 class Landing extends React.Component {
   constructor (props) {
@@ -50,16 +51,15 @@ class Landing extends React.Component {
 
   render () {
     return (
-        <div className='landing center px3 py4 white bg-gray bg-cover bg-center'>
-          <div className='page container'>
-
+        <div className='landing flex flex-center white bg-gray bg-cover bg-center'>
+          <div className='page container flex-center'>
                 <LandingLink to='/login' text='Login'
-                            hideWhenLoggedIn auth={this.props.auth}/>
+                            hideWhenLoggedIn auth={this.props.auth}/><br/>
                 <LandingLink to='/signup' text='Sign Up'
                             hideWhenLoggedIn auth={this.props.auth}/>
                 <LandingLink to='/apply' text='Apply'
                             roles={[USER_ROLE.APPLICANT]} auth={this.props.auth}/>
-	       </div>
+         </div>
         </div>
     );
   }
